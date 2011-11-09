95bd46c0065538b918492a751cf47995c19fc59d
Jeff Caddel <jcaddel@yahoo.com>
2011-11-09 09:00:49 -0700

    Better progress tracking of a directory upload

--- src/main/java/org/kuali/maven/wagon/PutThread.java
+++ src/main/java/org/kuali/maven/wagon/PutThread.java
@@ -10,43 +10,44 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
  */
 public class PutThread implements Runnable {
 
-	PutThreadContext context;
+    PutThreadContext context;
 
-	public PutThread() {
-		this(null);
-	}
+    public PutThread() {
+        this(null);
+    }
 
-	public PutThread(PutThreadContext context) {
-		super();
-		this.context = context;
-	}
+    public PutThread(PutThreadContext context) {
+        super();
+        this.context = context;
+    }
 
-	public void run() {
-		int offset = context.getOffset();
-		int length = context.getLength();
-		List<PutFileContext> list = context.getContexts();
-		RequestFactory factory = context.getFactory();
-		AmazonS3Client client = context.getClient();
-		for (int i = offset; i < offset + length; i++) {
-			if (i >= list.size()) {
-				break;
-			}
-			if (context.getHandler().isStopThreads()) {
-				break;
-			}
-			PutFileContext pc = list.get(i);
-			PutObjectRequest request = factory.getPutObjectRequest(pc);
-			pc.fireStart();
-			client.putObject(request);
-			pc.fireComplete();
-		}
-	}
+    public void run() {
+        int offset = context.getOffset();
+        int length = context.getLength();
+        List<PutFileContext> list = context.getContexts();
+        RequestFactory factory = context.getFactory();
+        AmazonS3Client client = context.getClient();
+        for (int i = offset; i < offset + length; i++) {
+            if (i >= list.size()) {
+                break;
+            }
+            if (context.getHandler().isStopThreads()) {
+                break;
+            }
+            PutFileContext pc = list.get(i);
+            pc.setProgress(null);
+            PutObjectRequest request = factory.getPutObjectRequest(pc);
+            // pc.fireStart();
+            client.putObject(request);
+            // pc.fireComplete();
+        }
+    }
 
-	public PutThreadContext getContext() {
-		return context;
-	}
+    public PutThreadContext getContext() {
+        return context;
+    }
 
-	public void setContext(PutThreadContext context) {
-		this.context = context;
-	}
+    public void setContext(PutThreadContext context) {
+        this.context = context;
+    }
 }
