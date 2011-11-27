c77b96fceb21d10f8dfe8ac8674665e7fe4e849e
Jeff Caddel <jcaddel@kuali.org>
2011-11-27 14:18:40 +0000

    Automated license header update

--- src/main/java/org/kuali/maven/wagon/TransferProgressFileInputStream.java
+++ src/main/java/org/kuali/maven/wagon/TransferProgressFileInputStream.java
@@ -1,11 +1,11 @@
-/*
- * Copyright 2004-2007 the original author or authors.
+/**
+ * Copyright 2004-2011 The Kuali Foundation
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.opensource.org/licenses/ecl2.php
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
