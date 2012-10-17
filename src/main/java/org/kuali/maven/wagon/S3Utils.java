51c1639873e08f5443eae015881775adeb068348
Jeff Caddel <jcaddel@yahoo.com>
2012-10-17 10:31:41 -0500

    Minor refactoring, better javadocs

--- src/main/java/org/kuali/maven/wagon/S3Utils.java
+++ src/main/java/org/kuali/maven/wagon/S3Utils.java
@@ -17,24 +17,35 @@ public class S3Utils {
 	private static final int MULTI_PART_UPLOAD_THRESHOLD = 100 * MEGABYTE;
 	private static final Logger log = LoggerFactory.getLogger(S3Utils.class);
 
+	/**
+	 * Upload a single file to Amazon S3. If the file is larger than 100MB a multi-part upload is used. This splits the file into multiple
+	 * smaller chunks with each chunk being uploaded in a different thread. Once all the threads have completed the file is reassembled on
+	 * Amazon's side as a single file again.
+	 */
 	public static final void upload(File file, PutObjectRequest request, AmazonS3Client client, TransferManager manager) {
-
 		// Store the file on S3
-		if (file.length() > MULTI_PART_UPLOAD_THRESHOLD) {
-			log.debug("Multi-part: " + file.getAbsolutePath());
-			// Use multi-part upload for large files
-			Upload upload = manager.upload(request);
-			try {
-				// Block and wait for the upload to finish
-				upload.waitForCompletion();
-			} catch (Exception e) {
-				throw new AmazonS3Exception("Unexpected error uploading file", e);
-			}
-		} else {
+		if (file.length() < MULTI_PART_UPLOAD_THRESHOLD) {
 			// Use normal upload for small files
 			client.putObject(request);
+		} else {
+			// Use multi-part upload for large files
+			blockingMultiPartUpload(file, request, manager);
 		}
+	}
 
+	/**
+	 * Use this to reliably upload large files. Amazon recommends using multi-part uploading on files larger than 100MB.
+	 */
+	public static final void blockingMultiPartUpload(File file, PutObjectRequest request, TransferManager manager) {
+		log.debug("Multi-part: " + file.getAbsolutePath());
+		// Use multi-part upload for large files
+		Upload upload = manager.upload(request);
+		try {
+			// Block and wait for the upload to finish
+			upload.waitForCompletion();
+		} catch (Exception e) {
+			throw new AmazonS3Exception("Unexpected error uploading file", e);
+		}
 	}
 
 }
