d2791987fb685d2617c0ec46213b69149bef1063
Jeff Caddel <jcaddel@yahoo.com>
2012-10-20 08:14:21 -0700

    Add the ability to determine total bucket size and number of objects

--- src/main/java/org/kuali/maven/wagon/S3Utils.java
+++ src/main/java/org/kuali/maven/wagon/S3Utils.java
@@ -16,13 +16,21 @@
 package org.kuali.maven.wagon;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.Upload;
 
@@ -34,6 +42,7 @@ public class S3Utils {
 	private static final int MEGABYTE = 1024 * KILOBYTE;
 	private static final int MULTI_PART_UPLOAD_THRESHOLD = 100 * MEGABYTE;
 	private static final Logger log = LoggerFactory.getLogger(S3Utils.class);
+	private static final int MAX_OBJECTS_PER_LISTING = 100000;
 
 	/**
 	 * Upload a single file to Amazon S3. If the file is larger than 100MB a multi-part upload is used. This splits the file into multiple
@@ -69,4 +78,32 @@ public class S3Utils {
 		}
 	}
 
+	public static final void getBucketStats(String accessKey, String secretKey, String bucketName) {
+		Assert.notNull(accessKey, "accessKey cannot be null");
+		Assert.notNull(secretKey, "secretKey cannot be null");
+		Assert.notNull(bucketName, "bucketName cannot be null");
+		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+		AmazonS3Client client = new AmazonS3Client(credentials);
+		getBucketStats(client, bucketName);
+	}
+
+	public static final void getBucketStats(AmazonS3Client client, String bucketName) {
+		if (!client.doesBucketExist(bucketName)) {
+			throw new IllegalArgumentException("Bucket '" + bucketName + "' does not exist");
+		}
+
+		ListObjectsRequest request = new ListObjectsRequest(bucketName, null, null, null, MAX_OBJECTS_PER_LISTING);
+		List<S3ObjectSummary> contents = new ArrayList<S3ObjectSummary>();
+		int count = 1;
+		while (true) {
+			log.info("Listing " + count);
+			ObjectListing listing = client.listObjects(request);
+			List<S3ObjectSummary> summaries = listing.getObjectSummaries();
+			contents.addAll(summaries);
+			if (!listing.isTruncated()) {
+				break;
+			}
+		}
+		log.info("Total objects: " + contents.size());
+	}
 }
