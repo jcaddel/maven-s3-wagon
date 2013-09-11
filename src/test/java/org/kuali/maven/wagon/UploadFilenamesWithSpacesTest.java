b5a96fa4c531e47f6f33b67c59ca1d6744d9d076
Jeff Caddel <jcaddel@yahoo.com>
2013-09-11 10:45:00 -0700

    Setup unit test for filenames with spaces

---
+++ src/test/java/org/kuali/maven/wagon/UploadFilenamesWithSpacesTest.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2010-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.maven.wagon;
+
+import org.apache.maven.wagon.authentication.AuthenticationInfo;
+import org.apache.maven.wagon.repository.Repository;
+import org.junit.Test;
+
+public class UploadFilenamesWithSpacesTest {
+
+	private static final String USERNAME = "AKIAJFD5IM7IPVVUEBNA";
+	private static final String PASSWORD = System.getProperty("secret.key");
+
+	// private static final Logger log = LoggerFactory.getLogger(S3WagonTest.class);
+
+	@Test
+	public void testPermissions() {
+		try {
+			AuthenticationInfo auth = new AuthenticationInfo();
+			auth.setUserName(USERNAME);
+			auth.setPassword(PASSWORD);
+			Repository repository = new Repository("kuali.external", "s3://maven.kuali.org/external");
+			S3Wagon wagon = new S3Wagon();
+			wagon.connect(repository, auth);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
